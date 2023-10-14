c3e5d215df3035792bd5416d057e95c042c7f117
Fabio Melo <fvmelo21@gmail.com>
2023-10-14 14:26:53 -0300

    commit 3

---
+++ Aulas_Python/aula5.py
@@ -0,0 +1,38 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+print('Multiplicação *', 10*10)
+print('Adição +', 10 + 10)
+print('Subtração -', 10 - 5)
+print('Divisão /', 10 / 2)
+
+# Concatenação
+print('Fabio' + ' ' + 'Vilela' + ' ' + 'Melo')
+print('Fabio', 'Vilela', 'Melo')
+
+# Conversão de tipo de dado
+print('Fabio' + ' ' + 'ele tem' + ' ' + '32' + ' ' + 'anos de idade')
+print('Fabio' + ' ' + 'ele tem' + ' ' + str(32) + ' ' + 'anos de idade')
+
+# Divisão
+print(10 / 3)
+print(10 //3)
+
+# Potenciação
+print(2 ** 10)
+
+# Resto da Divisão
+print(10 % 5)
+print(10 % 3)
+
+# Alterando a Precedência dos operadores (igual na Matemática)
+print(5+2*10)
+print((5+2)*10)
+
+print(20 * 'A')
+print(20*2)
+print('20' + 'A')
+print(20+2)
+
+
+
